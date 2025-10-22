0102bf18e6899383143a8097658775aa8494d8e7
Bhawar Gujral <Bhawargujral@gmail.com>
2025-10-22 22:00:14 +0000

    Enhance ServiceDao to implement getOne method and update App for service retrieval

--- learning-jdbc/src/main/java/com/frankmoley/lil/App.java
+++ learning-jdbc/src/main/java/com/frankmoley/lil/App.java
@@ -1,13 +1,11 @@
 package com.frankmoley.lil;
 
 import java.util.List;
+import java.util.Optional;
 
 import com.frankmoley.lil.data.dao.ServiceDao;
 import com.frankmoley.lil.data.entity.Service;
 
-/**
- * Hello world!
- */
 public class App {
     public static void main(String[] args) {
         ServiceDao serviceDao = new ServiceDao();
@@ -15,5 +13,9 @@ public class App {
         System.out.println("*** SERVICES ***");
         System.out.println("*** GET_ALL ***");
         services.forEach(System.out::println);
+
+        Optional<Service> service = serviceDao.getOne(services.get(0).getServiceId());
+        System.out.println("*** GET_ONE ***\n" + service.get());
+
     }
 }
